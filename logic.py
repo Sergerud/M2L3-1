6b7a6ab9e722bc8088eae13e52d5b4fb05cdeba8
Sergerud <serge@rudnya.pp.ru>
2025-06-16 02:41:39 +0300

    first commit

--- logic.py
+++ logic.py
@@ -1,4 +1,7 @@
 # Задание 2 - Импортируй нужные классы
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from urllib.request import urlopen
 
 class Question:
 
@@ -8,14 +11,36 @@ class Question:
         self.options = options
 
     # Задание 1 - Создай геттер для получения текста вопроса
+    @property #Декоратор, позволяющий вызвать метод по имени без скобок и добавляет в метод поле setter
+    def text(self):
+        return self.__text
     
     def gen_markup(self):
-        # Задание 3 - Создай метод для генерации Inline клавиатуры
+    
+        markup = InlineKeyboardMarkup() # Клавиатура, привязанная к сообщению
+        markup.row_width = len(self.options)
+        #print(markup)
+        for i,option in enumerate(self.options):
+            if i == self.__answer_id:
+                markup.add(InlineKeyboardButton(option, callback_data="correct",switch_inline_query_current_chat="links"))
+                
+            else:
+                markup.add(InlineKeyboardButton(option, callback_data="wrong",switch_inline_query_current_chat="links"))    
+       # Задание 3 - Создай метод для генерации Inline клавиатуры
         return markup
 
 # Задание 4 - заполни список своими вопросами
+
+
+#url = "./1.jpg"
+url1 = "Пишут мемы "+"https://yaustal.com/uploads/posts/2025-06/5fede51a6e_bolshoj-sbornik-smeshnyh-memov-i-kartinok-0.jpg"
+url2 = ""
+url3 = ""
+url4 = ""
 quiz_questions = [
-    Question("Что котики делают, когда никто их не видит?", 1, "Спят", "Пишут мемы"),
+    Question("Что котики делают, когда никто их не видит?", 1, "Спят", url1),
     Question("Как котики выражают свою любовь?", 0, "Громким мурлыканием", "Отправляют фото на Instagram", "Гавкают"),
-    Question("Какие книги котики любят читать?", 3, "Обретение вашего внутреннего урр-мирения", "Тайм-менеджмент или как выделить 18 часов в день для сна", "101 способ уснуть на 5 минут раньше, чем хозяин", "Пособие по управлению людьми")
+    Question("Какие книги котики любят читать?", 3, "Обретение вашего внутреннего урр-мирения", "Тайм-менеджмент или как выделить 18 часов в день для сна", "101 способ уснуть на 5 минут раньше, чем хозяин", "Пособие по управлению людьми"),
+    Question("Какие блюда готовят котики?", 2, "Сар-р-р-рдельки в собственном соку", "Скумбрия с хвостом", "Мышки в кошке")
 ]
+
--- main.py
+++ main.py
@@ -2,35 +2,53 @@
 import telebot
 from config import token
 # Задание 7 - испортируй команду defaultdict
+from collections import defaultdict
 from logic import quiz_questions
 
 user_responses = {} 
 # Задание 8 - создай словарь points для сохранения количества очков пользователя
-
+points = defaultdict(int) #словарь с дефолтным значением для любого нового ключа
 bot = telebot.TeleBot(token)
+count_quest=0
 
 def send_question(chat_id):
-    bot.send_message(chat_id, quiz_questions[user_responses[chat_id]].text, reply_markup=quiz_questions[user_responses[chat_id]].gen_markup())
+    
+    bot.send_message(chat_id, quiz_questions[user_responses[chat_id]].text, 
+                     reply_markup=quiz_questions[user_responses[chat_id]].gen_markup())
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
+    global count_quest, points
 
     if call.data == "correct":
+        points[call.message.chat.id] += 1
         bot.answer_callback_query(call.id, "Answer is correct")
+        
         # Задание 9 - добавь очки пользователю за правильный ответ
     elif call.data == "wrong":
         bot.answer_callback_query(call.id,  "Answer is wrong")
-      
+    
     # Задание 5 - реализуй счетчик вопросов
-
+    user_responses[call.message.chat.id]+=1  
     # Задание 6 - отправь пользователю сообщение с количеством его набранных очков, если он ответил на все вопросы, а иначе отправь следующий вопрос
+    
+    if user_responses[call.message.chat.id]>=len(quiz_questions):
+        bot.send_message(call.message.chat.id, "The end")
+        bot.send_message(call.message.chat.id, "Правильных ответов "+str(points[call.message.chat.id]))
+    else:
+        send_question(call.message.chat.id)
 
-
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start'])  #Обработчик команды start
 def start(message):
     if message.chat.id not in user_responses.keys():
         user_responses[message.chat.id] = 0
         send_question(message.chat.id)
-
+    else:    
+        points.clear
+        count_quest=0
+        user_responses[message.chat.id] = 0
+        send_question(message.chat.id)
+        #print(count_quest)
+        
 
 bot.infinity_polling()
